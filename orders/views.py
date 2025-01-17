2fae2dc0fa1f509742d18715bac0ff31c1db28c0
aleks <aleksejkrimsi@gmail.com>
2025-01-18 00:15:22 +0300

    order_update

--- orders/views.py
+++ orders/views.py
@@ -40,6 +40,7 @@ class OrderDetailView(DetailView):
 
     
 class OrderCreateView(CreateView):
+    """Cоздание заказа"""
     model = Order
     form_class = OrderAddItemsForm
     template_name = 'orders/order_form.html'
@@ -65,11 +66,47 @@ class OrderCreateView(CreateView):
 
 
 class OrderUpdateView(UpdateView):
-    """Изменение заказа"""
+    """Редактирование заказа"""
     model = Order
     form_class = OrderAddItemsForm
-    template_name = 'orders/order_form.html'
-    success_url = reverse_lazy('order_list')
+    template_name = 'orders/order_form_update.html'
+    success_url = reverse_lazy('orders:order_list')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        order = self.object
+        selected_item_ids = [item.get('id', None) for item in order.items if isinstance(item, dict)]
+        context['selected_item_ids'] = selected_item_ids
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        selected_items = self.request.POST.getlist('selected_items')
+        status = self.request.POST.get('status')
+
+        # Обновление списка блюд
+        new_items = []
+        for item_id in selected_items:
+            try:
+                item = Item.objects.get(pk=item_id)
+                new_items.append({
+                    'id': str(item.pk),
+                    'name': item.name,
+                    'price': float(item.price),
+                })
+            except Item.DoesNotExist:
+                continue
+
+        # Обновляем поле items
+        self.object.items = new_items
+        self.object.calculate_total_price()
+
+        # Обновление статуса
+        if status:
+            self.object.status = status
+
+        self.object.save()
+        return response
 
 
 class OrderDeleteView(DeleteView):
