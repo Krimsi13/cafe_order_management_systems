70c0515f445832f4bf3a8253ce4fe727bc869128
aleks <aleksejkrimsi@gmail.com>
2025-01-19 22:08:36 +0300

    tests_ordercreate_&_ordersearchapi

--- api_orders/tests.py
+++ api_orders/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from orders.models import Item, Order
+from api_orders.serializers import OrderSerializer
 
-# Create your tests here.
+
+class OrderSearchApiTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.item1 = Item.objects.create(name='Пицца Маргарита', price=500.00)
+        cls.item2 = Item.objects.create(name='Салат Цезарь', price=300.00)
+
+        cls.order1 = Order.objects.create(
+            table_name='Table 1',
+            items=[{'name': 'Пицца Маргарита', 'price': 500.00}]
+        )
+        cls.order1.status = 'waiting'
+        cls.order1.save()
+
+        cls.order2 = Order.objects.create(
+            table_name='Table 2',
+            items=[{'name': 'Салат Цезарь', 'price': 300.00}]
+        )
+        cls.order2.status = 'ready'
+        cls.order2.save()
+
+    def test_search_by_table_name(self):
+        url = reverse('api:api_order_search')
+        response = self.client.get(f'{url}?table_name=Table%201')
+        self.assertEqual(response.status_code, 200)
+        expected_serializer = OrderSerializer(Order.objects.filter(table_name='Table 1'), many=True)
+        self.assertEqual(expected_serializer.data, response.json())
+
+    def test_search_by_status(self):
+        url = reverse('api:api_order_search')
+        response = self.client.get(f'{url}?status=waiting')
+        self.assertEqual(response.status_code, 200)
+        expected_serializer = OrderSerializer(Order.objects.filter(status='waiting'), many=True)
+        self.assertEqual(expected_serializer.data, response.json())
+
+    def test_search_by_both_params(self):
+        url = reverse('api:api_order_search')
+        response = self.client.get(f'{url}?table_name=Table%202&status=ready')
+        self.assertEqual(response.status_code, 200)
+        expected_serializer = OrderSerializer(Order.objects.filter(table_name='Table 2').filter(status='ready'), many=True)
+        self.assertEqual(expected_serializer.data, response.json())
+
+    def test_no_results(self):
+        url = reverse('api:api_order_search')
+        response = self.client.get(f'{url}?table_name=Nonexistent&status=pending')
+        self.assertEqual(response.status_code, 400)
+        
--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
+from django.urls import reverse
+from orders.models import Item, Order
+from orders.forms import OrderAddItemsForm
 
-# Create your tests here.
+class OrderCreateViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.item1 = Item.objects.create(name='Пицца Маргарита', price=500.00)
+        cls.item2 = Item.objects.create(name='Салат Цезарь', price=300.00)
+        cls.invalid_item_id = 100  # ID несуществующего товара
+    
+    def test_get_create_view(self):
+        url = reverse('orders:order_create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'orders/order_form.html')
+        self.assertIsInstance(response.context['form'], OrderAddItemsForm)
+    
+    def test_post_create_view_with_valid_items(self):
+        data = {
+            'table_name': 'Table 1',
+            'selected_items': [str(self.item1.pk), str(self.item2.pk)],
+        }
+        url = reverse('orders:order_create')
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('orders:order_list'))
+        order = Order.objects.last()
+        self.assertEqual(order.table_name, 'Table 1')
+        self.assertEqual(len(order.items), 2)  # Убираем json.loads(), так как items уже является списком
+        self.assertIn({'name': 'Пицца Маргарита', 'price': 500.00}, order.items)
+        self.assertIn({'name': 'Салат Цезарь', 'price': 300.00}, order.items)
+        self.assertEqual(order.total_price, 800.00)
+    
+    def test_post_create_view_without_items(self):
+        data = {
+            'table_name': 'Table 1',
+            'selected_items': [],
+        }
+        url = reverse('orders:order_create')
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('orders:order_list'))
+        order = Order.objects.last()
+        self.assertEqual(order.table_name, 'Table 1')
+        self.assertEqual(order.items, [])
+        self.assertEqual(order.total_price, 0.00)
